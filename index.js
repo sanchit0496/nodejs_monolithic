16dd38a2bd8a0292e1016edd53846d69993b8e9e
Sanchit <32457437+sanchit0496@users.noreply.github.com>
2024-02-03 03:01:08 +0530

    using env

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const app = require('./app');
 const sequelize = require('./config/database');
+require('dotenv').config();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.RDS_PORT || 3000;
+console.log('process.env.RDS_PORT', process.env.RDS_PORT)
 
 
 sequelize.sync({ alter: true }) // This will check what is the current state of the table in the database (which columns it has, what are their data types, etc), and then perform the necessary changes in the table to make it match the model.
@@ -9,7 +11,7 @@ sequelize.sync({ alter: true }) // This will check what is the current state of 
         console.log('Database synced');
         // ... start your server here
         app.listen(PORT, () => {
-            console.log(`Server running on port ${PORT}`);
+            console.log(`Server running on RDS_PORT ${PORT}`);
         });
     })
     .catch((err) => {
