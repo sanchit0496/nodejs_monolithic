609e0c37578e0c6e36c2baead9f1bc21b9eaf2f3
Sanchit <32457437+sanchit0496@users.noreply.github.com>
2024-02-03 20:00:08 +0530

    added the subchapterread module to the app

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const logger = require("./logger"); // Make sure the path is correct
 const userRoutes = require('./routes/appUserRoutes');
 const progressRoutes = require('./routes/progressRoutes')
+const subchapterRoutes = require('./routes/subChapterReadRoutes')
 const { swaggerSpec, swaggerUi } = require('./swaggerConfig'); // Adjust the path if necessary
 
 app.use(express.json()); // For parsing application/json
@@ -20,6 +21,7 @@ app.use((req, res, next) => {
 // Use the user routes for any requests to '/users'
 app.use('/appusers', userRoutes);
 app.use('/progress', progressRoutes);
+app.use('/subchapterread', subchapterRoutes);
 
 
 // Export the app for use in index.js
